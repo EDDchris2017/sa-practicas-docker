cc4af17204b1e83287122c6ec6439782b5a445ae
Chris <crhalfa09@gmail.com>
2020-10-02 00:09:18 -0600

    Creacion de Docker Compose y prueba Server Flask

---
+++ BD/data.sql
@@ -0,0 +1,15 @@
+USE TIENDA;
+
+CREATE TABLE VIDEOJUEGOS
+(
+    id int PRIMARY KEY,
+    nombre varchar(30),
+    genero varchar(20),
+    plataforma varchar(20)
+);
+
+INSERT INTO VIDEOJUEGOS VALUES (1,"Super Mario Galaxy","Plataformas","Wii");
+INSERT INTO VIDEOJUEGOS VALUES (2,"Forza Horizon 4","Conduccion","Xbox One");
+INSERT INTO VIDEOJUEGOS VALUES (3,"God of War","Accion/Aventuras","PS4");
+INSERT INTO VIDEOJUEGOS VALUES (4,"Residenti evil 7","Survival Horror","Multiplataforma");
+INSERT INTO VIDEOJUEGOS VALUES (5,"Gears of War 5","Accion","Xbox One");
