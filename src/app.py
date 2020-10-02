8ca8109321aa6fe2d7a4607aae6348c8facbbcfd
Chris <crhalfa09@gmail.com>
2020-10-02 01:49:32 -0600

    Verificar Servidor Mysql

--- src/app.py
+++ src/app.py
@@ -1,9 +1,27 @@
 
 from flask import Flask
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 
+def conexionBD():
+    connection = mysql.connector.connect(host='mysql',
+                                        database='TIENDA',
+                                        user='sapractica',
+                                        password='sapractica')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Conectando a la BD ... ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select * from VIDEOJUEGOS")
+        record = cursor.fetchone()
+        print("Lista de videjuegos: ", record)
+    cursor.close()
+    connection.close()
+    print("MySQL connection is closed")
 
 @app.route('/')
-def hello():
-    return 'Servidor Flask Practica 8 Activo 201504100'
+def servicio():
+    conexionBD()
+    return 'Servidor Flask Practica 8 Activo **201504100**'
